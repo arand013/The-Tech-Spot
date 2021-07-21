f54abe93d6280687bb4f26cd6f6fd8a44b6508f3
Alexander Aranda <Alexandermarkets1@gmail.com>
2021-07-20 22:40:52 -0700

    Model/ index.js : Comment associations

--- develop/models/index.js
+++ develop/models/index.js
@@ -12,4 +12,13 @@ User.hasMany(Comment, {
     foreignKey: 'user_id'
   });
 
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+  
+  });
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+  
+  });
+
 
