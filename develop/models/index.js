15cdfb253334409d7a699f1dfbf4bc219c6cda55
Alexander Aranda <Alexandermarkets1@gmail.com>
2021-07-20 22:42:59 -0700

    Model/ index.js : module exports linked

--- develop/models/index.js
+++ develop/models/index.js
@@ -6,27 +6,29 @@ const Comment = require('./Comment');
 // This will CREATE associations
 User.hasMany(Post, {
     foreignKey: 'user_id'
-  });
+});
 
 User.hasMany(Comment, {
     foreignKey: 'user_id'
-  });
+});
 
 Comment.belongsTo(User, {
     foreignKey: 'user_id',
-  
-  });
+
+});
 Comment.belongsTo(Post, {
     foreignKey: 'post_id',
-  
-  });
+
+});
 
 Post.belongsTo(User, {
     foreignKey: 'user_id',
-  
-  });
+
+});
 
 Post.belongsTo(Comment, {
     foreignKey: 'post_id',
-  
-  });
+
+});
+
+module.exports = { User, Post, Comment };
