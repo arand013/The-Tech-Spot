6cd898e1416d6348961de608b51b27e443e6cdff
Alexander Aranda <Alexandermarkets1@gmail.com>
2021-07-20 22:40:29 -0700

    Model/ index.js : User associations

--- develop/models/index.js
+++ develop/models/index.js
@@ -2,3 +2,14 @@
 const Post = require('./Post');
 const User = require('./User');
 const Comment = require('./Comment');
+
+// This will CREATE associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+  });
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+  });
+
+
